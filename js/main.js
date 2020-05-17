6cfad8c115d6e644349309f2e7e91c920847bfed
BrandanTD <brandantyler@att.net>
2020-05-17 05:51:07 -0500

    main socials & integration added

--- js/main.js
+++ js/main.js
@@ -101,12 +101,64 @@ $(window).on('load', function() {
 	}
 
 	function populateUser(user) {
+
+		var rankColors = {
+			'ORG': '#0974fe',
+			'CEO': '#FFD700',
+			'Owner': 'white',
+			'Staff': '#00f8ff'
+		};
 		
 		//Set Items
 		$('title').html(
 			'RM | ' + user.name.toUpperCase()
 		)
-		
+
+		$('#player-name').html(
+			user.name.toUpperCase()
+		)
+
+		$('#player-tier').html(
+			'<div id="rank-circle"></div>' +
+			user.rank
+		)
+
+		$('#player-tier').css(
+			'border-color',
+			rankColors[user.rank]
+		)
+
+		$('#rank-circle').css(
+			'background-color',
+			rankColors[user.rank]
+		)
+
+		$('.custom-background').css(
+			'background-image',
+			user.background
+		);
+
+		$('#player-discord').attr(
+			'href',
+			user.discord
+		);
+
+		$('#player-youtube').attr(
+			'href',
+			user.youtube
+		);
+
+		$('#player-twitch').attr(
+			'href',
+			user.twitch
+		);
+
+		$('#player-instagram').attr(
+			'href',
+			user.instagram
+		);
+
+			
 		$.ajax({
 			url: user.instagram + "?__a=1",
 			cache: false
@@ -128,58 +180,58 @@ $(window).on('load', function() {
 		var userName = GetURLParameter('id');
 
 		var ranks = {
-			'rememberclan': { 
-				name: 'rememberclan',
-				rank: 'brand',
-				discord: 'https://discord.gg/BYDmACR',
-				youtube: 'https://www.youtube.com/channel/UC45QQL2hM3V5JbbqSib2blw',
-				twitch: 'https://www.twitch.tv/0Deacon',
-				instagram: 'https://www.instagram.com/rm.deacon/',
-				background: ''
+			'rc': { 
+				name: 'Team Remember®',
+				rank: 'ORG',
+				discord: 'https://discord.gg/5Kbj3gZ',
+				youtube: 'https://www.youtube.com/channel/UCp1tD7Ee8pT_xL3uI67Qxow',
+				twitch: 'https://www.twitch.tv/rememberclan',
+				instagram: 'https://www.instagram.com/rememberclan/',
+				background: 'url(img/rc-default/bg.png)'
 			},
 			'rm.viral': { 
 				name: 'viral',
-				rank: 'founder',
-				discord: 'https://discord.gg/BYDmACR',
-				youtube: 'https://www.youtube.com/channel/UC45QQL2hM3V5JbbqSib2blw',
-				twitch: 'https://www.twitch.tv/0Deacon',
-				instagram: 'https://www.instagram.com/rm.deacon/',
-				background: ''
+				rank: 'CEO',
+				discord: 'https://discord.gg/5Kbj3gZ',
+				youtube: 'https://www.youtube.com/channel/UCp1tD7Ee8pT_xL3uI67Qxow',
+				twitch: 'https://www.twitch.tv/rememberclan',
+				instagram: 'https://www.instagram.com/rm.viral/',
+				background: 'url(img/rc-default/bg.png)'
 			},
 			'rm.kad': { 
 				name: 'kad',
-				rank: 'owner',
-				discord: 'https://discord.gg/BYDmACR',
-				youtube: 'https://www.youtube.com/channel/UC45QQL2hM3V5JbbqSib2blw',
-				twitch: 'https://www.twitch.tv/0Deacon',
-				instagram: 'https://www.instagram.com/rm.deacon/',
-				background: ''
+				rank: 'Owner',
+				discord: 'https://discord.gg/5Kbj3gZ',
+				youtube: 'https://www.youtube.com/channel/UCp1tD7Ee8pT_xL3uI67Qxow',
+				twitch: 'https://www.twitch.tv/rememberclan',
+				instagram: 'https://www.instagram.com/rm.kad/',
+				background: 'url(img/rc-default/bg.png)'
 			},
 			'rmitsbrandan': { 
-				name: 'brandan',
-				rank: 'owner',
-				discord: 'https://discord.gg/BYDmACR',
-				youtube: 'https://www.youtube.com/channel/UC45QQL2hM3V5JbbqSib2blw',
-				twitch: 'https://www.twitch.tv/0Deacon',
-				instagram: 'https://www.instagram.com/rm.deacon/',
-				background: ''
+				name: 'STYLEZ ♆',
+				rank: 'Owner',
+				discord: 'https://discord.com/invite/6ekJGJh',
+				youtube: 'https://www.youtube.com/serstylez',
+				twitch: 'https://www.twitch.tv/serstylez',
+				instagram: 'https://www.instagram.com/rmitsbrandan/',
+				background: 'url(img/rc-default/bg.png)'
 			},
 			'rm.deacon': { 
 				name: 'deacon',
-				rank: 'staff',
+				rank: 'Staff',
 				discord: 'https://discord.gg/BYDmACR',
 				youtube: 'https://www.youtube.com/channel/UC45QQL2hM3V5JbbqSib2blw',
 				twitch: 'https://www.twitch.tv/0Deacon',
 				instagram: 'https://www.instagram.com/rm.deacon/',
-				background: ''
+				background: 'url(players/rm.deacon/bg.jpg)'
 			}
 		}
 
 		if(ranks[userName]) {
-			window.history.pushState(null, null, "/u/" + userName);
+			//window.history.pushState(null, null, "/u/" + userName);
 			populateUser(ranks[userName]);
 		} else {
-			window.history.pushState(null, null, "/u/UNKNOWN");
+			//window.history.pushState(null, null, "/u/UNKNOWN");
 			$('title').html(
 				'UNKNOWN'
 			)
